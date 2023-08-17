f1c36a7c867c362342279814d713b525af42ad7a
ericsson <ericssontee.eb@gmail.com>
2023-08-17 22:57:55 +0800

    Add Image upload

---
+++ API/Extensions/MappingProfiles.cs
@@ -0,0 +1,15 @@
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Extensions
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<UpdateProductDto, Product>();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,14 @@ using System.Text;
 using Microsoft.OpenApi.Models;
 using API.Entities;
 using API.Services;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -97,6 +99,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<TokenService>();
 builder.Services.AddScoped<PaymentService>();
+builder.Services.AddScoped<ImageService>();
 
 var app = builder.Build();
 
